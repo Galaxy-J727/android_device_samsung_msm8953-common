70eca7e2048a2ce951fdc5d430528b3d1c45ed44
minz1 <emerytang@gmail.com>
2018-02-27 21:10:27 -0600

    msm8953-common: blob libexifa/libjpega

--- product/camera.mk
+++ product/camera.mk
@@ -3,10 +3,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
 
-# Samsung Camera Libraries
-PRODUCT_PACKAGES += \
-    libjpega
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/vendor/etc/permissions/android.hardware.camera.front.xml \
